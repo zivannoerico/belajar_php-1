8a3627a4356f527ed598f3df21fc4f8dcd483f39
Retno Ires <fairy.edensor@gmail.com>
2025-01-06 11:59:27 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H3>
+        <?php 
+            $var1 = 20;
+            echo "Hello World!!! Ini adalah halaman Pertama PHP <br/>";
+            echo $var1; 
+        ?>
+    </H3>
+</body>
+</html>
