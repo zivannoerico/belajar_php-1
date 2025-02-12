e4876824c6abe36e61cd8cd51756d922f88d76f3
zivannoerico <fanoerico@gmail.com>
2025-02-12 17:12:51 +0700

    tugas3_php

--- form/index.php
+++ form/index.php
@@ -8,7 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <style>
         td {
             padding : 1em;
@@ -19,10 +18,10 @@
     <form action="proses.php" method="POST">
         <table border=1 style="border-collapse : collapse; width : 50%; ">
             <tr>
-                <td>NIS </td>
+                <td>NISN </td>
                 <td>:</td>
                 <td>
-                    <input type="text" name="nis" id=""  />
+                    <input type="text" name="NISN" id=""  />
                 </td>
             </tr>
             <tr>
@@ -100,6 +99,6 @@
 
 
     </form>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
